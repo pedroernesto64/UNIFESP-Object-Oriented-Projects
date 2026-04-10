18652ddb3f983712c5b095b34ef3fb772cd1e71e
pedroernesto64 <pedroernestodp@gmail.com>
2026-04-10 15:32:10 -0300

    Proxy and Adapter

--- Atividade Aula Proxy e Adapter/Adapter.java
+++ Atividade Aula Proxy e Adapter/Adapter.java
@@ -1,16 +1,13 @@
-// Interface comum do sistema
 interface Notification {
     void send(String message);
 }
 
-// Classe do Fabricante (Legada/Externa) com interface incompatível
 class ExternalSMSAPI {
     public void executeSend(String msg, int priority) {
         System.out.println("API Externa enviando: " + msg + " (Prioridade: " + priority + ")");
     }
 }
 
-// Adaptador para integrar a API externa ao nosso sistema
 class SMSAdapter implements Notification {
     private ExternalSMSAPI api;
 
@@ -20,7 +17,6 @@ class SMSAdapter implements Notification {
 
     @Override
     public void send(String message) {
-        // Traduz a chamada 'send' para o método específico da API 'executeSend'
         api.executeSend(message, 1);
     }
 }
