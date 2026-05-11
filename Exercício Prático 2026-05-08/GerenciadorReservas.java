0f1117319ed50099959bb3ad44b0d3a78b9e9419
I3erfs <148400713+I3erfs@users.noreply.github.com>
2026-05-11 20:38:40 -0300

    Conexão ao Strategy em GerenciadorReservas.java

--- Exercício Prático 2026-05-08/GerenciadorReservas.java
+++ Exercício Prático 2026-05-08/GerenciadorReservas.java
@@ -1,12 +1,18 @@
 class GerenciadorReserva {
+  private PoliticaColisao strategy;
+
   private GerenciadorReserva() {}          // proíbe clientes de chamarem new GerenciadorReserva()‏
   
   private static GerenciadorReserva instance;           // instância única da classe
   
   public static GerenciadorReserva getInstance() {
-    if(instance == null)        // primeira vez que chama-se getInstance
+    if(instance == null)        // primeira vez que se chama getInstance
       instance = new GerenciadorReserva();
     return instance;
+  }
+
+  public void setPoliticaStrategy(PoliticaColisao strategy){
+    this.strategy = strategy;
   }
   
   public void criarReserva() {
