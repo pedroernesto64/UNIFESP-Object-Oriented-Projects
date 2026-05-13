14809a7aac9980326b4afc6e4d9499d299fd893e
I3erfs <148400713+I3erfs@users.noreply.github.com>
2026-05-12 22:35:56 -0300

    definição do método ToString

--- Exercício Prático 2026-05-08/Reserva.java
+++ Exercício Prático 2026-05-08/Reserva.java
@@ -1,23 +1,44 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Reserva {
     private static int nextId = 1;
     private final int id;
     private final Usuario usuario;
     private final Sala sala;
-    private final String horario;
+    private final LocalDate data;
+    private final LocalTime horarioInicio;
+    private final LocalTime horarioTermino;
+    
 
-    public Reserva(Usuario usuario, Sala sala, String horario) {
+    public Reserva(Usuario usuario, Sala sala, LocalDate data, LocalTime horarioInicio, LocalTime horarioTermino) {
+        if (fim.isBefore(inicio) || fim.isEqual(inicio)) {
+        throw new IllegalArgumentException("Erro: O horário de término deve ser após o horário de início.");
+    }
         this.id = nextId++;
         this.usuario = usuario;
         this.sala = sala;
-        this.horario = horario;
+        this.data = data;
+        this.horarioInicio = horarioInicio;
+        this.horarioTermino = horarioTermino;
     }
 
     public int getId() { return id; }
     public Usuario getUsuario() { return usuario; }
     public Sala getSala() { return sala; }
-    public String getHorario() { return horario; }
+    public LocalDate getData() { return data; }
+    public LocalTime getInicio() { return horarioInicio; }
+    public LocalTime getTermino() { return horarioTermino; }
 
+    public boolean temConflito(Reserva outra) {
+        if (!this.data.equals(outra.getData())) return false;
+        return (this.horarioInicio.isBefore(outra.getTermino()) && 
+                this.horarioTermino.isAfter(outra.getInicio()));
+    }
+
+    @Override
     public String toString() {
-        return "Reserva#" + id + "(" + usuario + ", " + sala.getClass().getSimpleName() + ", " + horario + ")";
+        return String.format("Reserva [%s] - Data: %s | %s às %s | Sala: %d | Usuário: %s", 
+            this.id, this.data, this.horarioInicio, this.horarioTermino, this.sala.getNumero(), this.usuario.getNome());
     }
 }
